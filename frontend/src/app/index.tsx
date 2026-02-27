51b0fbe1a622f556c215cea5c4257eed6d30a395
tejasvi <teju.lovegood@gmail.com>
2026-02-27 13:12:22 -0600

    set up

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View,  StyleSheet } from 'react-native';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text style={styles.text}>Home screen</Text>
     </View>
   );
 }
@@ -11,7 +11,12 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
   },
 });
+
