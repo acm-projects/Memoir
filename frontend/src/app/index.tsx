271bf9bebd8e2620ee18728b7b0a2ae718553c36
Jiya Khurana <jiyakhurana2102@gmail.com>
2026-03-04 20:51:10 -0600

    Loading Page first draft

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,21 +1,17 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import EnvelopeLoading from '../components/EnvelopeLoading';
+import { View, Text } from 'react-native';
 
 export default function Index() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  if (isLoading) {
+    return <EnvelopeLoading onComplete={() => setIsLoading(false)} />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl font-bold">Welcome to the App!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-});
