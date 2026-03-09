8180da48bffce0017fdd79d2ce73442e30e85e91
Jiya Khurana <jiyakhurana2102@gmail.com>
2026-03-08 23:20:59 -0500

    got it to work

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -1,16 +1,15 @@
 import { Stack } from "expo-router";
- import { useFonts, Montaga_400Regular } from '@expo-google-fonts/montaga';
- import { Inter_400Regular, } from '@expo-google-fonts/inter';
-
-
+import { useFonts, Montaga_400Regular } from '@expo-google-fonts/montaga';
+import { Inter_400Regular } from '@expo-google-fonts/inter';
+import "./global.css";
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
+  const [loaded] = useFonts({
     'Montaga': Montaga_400Regular,
     'Inter': Inter_400Regular,
   });
-  return <Stack screenOptions={{ headerShown: false }}/>;
-import "./global.css"
-export default function RootLayout() {
-  return <Stack />;
+
+  if (!loaded) return null;
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
